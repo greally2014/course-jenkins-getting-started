1112b492731517e0cd25e28d90bfd6ed93dad7fb
Greally <Daniel.Greally@aspect.com>
2021-07-05 18:06:16 +0100

    refactor

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,30 +1,20 @@
 pipeline {
     agent any
-    triggers { pollSCM('* * * * *') }
+
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
-            }            
+                git branch: 'main', url: 'https://github.com/greally2014/jgsu-spring-petclinic.git'
+            }
         }
         stage('Build') {
             steps {
-                sh './mvnw clean package'
-                //sh 'false' // true
+                bat "./mvnw clean package"
             }
-        
             post {
                 always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
+                    junit '*target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
