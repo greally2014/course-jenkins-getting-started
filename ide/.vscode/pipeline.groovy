81caaea961ef00b81edadd70cc8418d67c1d4251
Greally <Daniel.Greally@aspect.com>
2021-07-05 18:05:31 +0100

    chore: add pipeline.groovy file to .vscode/

---
+++ ide/.vscode/pipeline.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/greally2014/jgsu-spring-petclinic.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                bat "./mvnw clean package"
+            }
+            post {
+                always {
+                    junit '*target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
